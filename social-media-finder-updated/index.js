aa4521475a5dc96cb41877eb33eabe9d326a5c89
kalievil <42854360+kalievil@users.noreply.github.com>
2025-04-18 00:54:15 +0200

    Delete social-media-finder-updated directory

--- social-media-finder-updated/index.js
+++
@@ -1,9 +0,0 @@
-// Simple root endpoint to verify the server is running
-module.exports = (req, res) => {
-  res.json({
-    message: 'Social Media Finder API is running',
-    endpoints: {
-      search: '/api/search-image'
-    }
-  });
-}; 
